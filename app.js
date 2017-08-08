77f82d23de563fdbf7e7646a7f1d87b5ad0f792f
Taylor Cox <taylorashtoncox@gmail.com>
2017-08-08 09:25:19 -0400

    Hooked up database and changed names of mustache tags on directory-last page

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const express = require('express')
 const path = require('path')
 const mustacheExpress = require('mustache-express')
 const data = require('data.js')
+const pgp = require('pg-promise')()
+const database = pgp({ database: 'robot-users' })
+const query = 'SELECT * FROM users'
+const queryuser = 'SELECT * FROM users WHERE id = ${id}'
+
+// SQL Table Creation
+// CREATE TABLE users (
+  // "id" SERIAL PRIMARY KEY,
+  // "username" VARCHAR(100) NOT NULL,
+  // "imageurl" VARCHAR(100) NULL,
+  // "email" VARCHAR(100) NULL,
+  // "university" VARCHAR(100) NULL,
+  // "street_number" VARCHAR(100) NULL,
+  // "address" VARCHAR(100) NULL,
+  // "city" VARCHAR(100) NULL,
+  // "state" VARCHAR(100) NULL,
+  // "job" VARCHAR(100) NULL,
+  // "company" VARCHAR(100) NULL,
+  // "postal_code" VARCHAR(100) NULL,
+  // "year_built" VARCHAR(100) NULL,
+  // "next_service_date" VARCHAR(100) NULL,
+  // "is_active" BOOLEAN
+// )
 
 const app = express()
 
@@ -11,12 +34,17 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/', function(req, res) {
-  res.render('directory-list', data)
+  // res.render('directory-list', data)
+  database.any(query).then(rows => {
+    // rows.forEach()
+    console.log(rows)
+    res.render('directory-list', { users: rows })
+  })
 })
 
-app.get('/users/:username', (req, res) => {
+app.get('/users/:id', (req, res) => {
   const profileData = {
-    username: req.params.username
+    username: req.params.id
   }
 
   function findUser(user) {
